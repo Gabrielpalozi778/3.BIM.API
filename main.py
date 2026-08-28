31d641237b4d02521fb6e752da949c53bf3d8218
Gabrielpalozi778 <palozigabriel777@gmail.com>
2026-08-28 17:32:28 -0300

    Aula 01 - Exercicio - GET / id e DELETE

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 from database import Base, engine, get_db
 from models import ProdutoDB
 from schemas import ProdutoCreate, ProdutoResponse
+from fastapi import HTTPException
 
 Base.metadata.create_all(bind=engine) # cria as tabelas, se ainda não existirem
 app = FastAPI()
@@ -19,3 +20,20 @@ def criar_produto(produto: ProdutoCreate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(novo_produto)
     return novo_produto
+
+# GET /produtos/{id} -> retorna um único produto pelo id
+@app.get('/produtos/{produto_id}', response_model=ProdutoResponse)
+def obter_produto(produto_id: int, db: Session = Depends(get_db)):
+    produto = db.query(ProdutoDB).filter(ProdutoDB.id == produto_id).first()
+    if produto is None:
+        raise HTTPException(status_code=404, detail='Produto não encontrado')
+    return produto
+
+# DELETE /produtos/{id} -> remove um produto do banco de dados
+@app.delete('/produtos/{produto_id}', status_code=204)
+def remover_produto(produto_id: int, db: Session = Depends(get_db)):
+    produto = db.query(ProdutoDB).filter(ProdutoDB.id == produto_id).first()
+    if produto is None: 
+        raise HTTPException(status_code=404, detail='Produto não encontrado')
+    db.delete(produto)
+    db.commit()
